41c8e46916865b7208058071f187750ca867d956
Cyrine SASSI <cyrine.sassi@talan.com>
2023-10-05 15:29:57 +0200

    Deploying web services in Azure

---
+++ vars.tf
@@ -0,0 +1,27 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this project"
+  default     = "Azuredevops"
+}
+
+variable "location" {
+  description = "The Azure region in which all resources in this example should be created"
+  default     = "West Europe"
+}
+
+variable "vm_number" {
+  description = "The number of the VMs"
+  default     = 3
+}
+
+variable "username" {
+  description = "The admin-Username for VM"
+  default     = "Admin-username"
+}
+
+variable "password" {
+  description = "The admin-password for VM"
+  default     = "Admin@Password1"
+}
